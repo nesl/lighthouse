785f2a5084d74e96e5c59ae0b90d6f06a177bf08
rshea <rshea@73ef524f-9218-0410-8865-d28d8c259d7e>
2006-07-19 18:37:28 +0000

    New build system using OCamlMakefile

--- src/memCheck/addAnnotations.ml
+++ src/memCheck/addAnnotations.ml
@@ -11,16 +11,17 @@ open Cil
 module E = Errormsg
 
 let dbg_verbose = ref false
-             
-let annotations = (Hashtbl.create 5)
 
-class addAnnotationsVisitor = object (self)
-  inherit nopCilVisitor
+let addAttributes annotations global =
 
-end
+  (* Extract fundec and varinfo from global *)
+  let (fdop, vop) = 
+    match global with
+        GVarDecl (v, _) -> (None, Some v)
+      | GFun (f, _) -> (Some f, Some f.svar)
+      | _ -> (None, None)
+  in
 
-let addAttributes (gl: global list) (g: global) =
-      
   (* Return a new type that is the old type with an additional attribute set *)
   let newType t attr = 
     match t with
@@ -36,13 +37,7 @@ let addAttributes (gl: global list) (g: global) =
       | TBuiltin_va_list a -> TBuiltin_va_list (addAttribute attr a)
   in
 
-  let (fdop, vop) = 
-    match g with
-        GVarDecl (v, _) -> (None, Some v)
-      | GFun (f, _) -> (Some f, Some f.svar)
-      | _ -> (None, None)
-  in
-
+  (* Update the varinfo with a new annotations if needed *)
   let newFunTypeOp = 
     match vop with
         Some v ->
@@ -71,7 +66,7 @@ let addAttributes (gl: global list) (g: global) =
                         | None -> returnType
                     in
 
-                      
+
                     (* Assuming that the length of tl is the same as tlop *)
                     let formalTypeLOp = 
                       match formalTypeLOp with
@@ -117,92 +112,93 @@ let addAttributes (gl: global list) (g: global) =
           v.vtype <- nft;
           if !dbg_verbose then
             ignore (printf "Prototype update for extern func %s: %a\n" v.vname d_type v.vtype);
-           ()
-          
-      
+          ()
+
+
+(** Get annotations from a file
+  * @todo Update this to read from a file rather than hardcode the values
+  * *)
+let getAnnotations () =
+    
+  let annotations = (Hashtbl.create 5) in
+
+    Hashtbl.add annotations "ker_malloc" 
+      [Some "sos_claim"; None; None];
+
+    Hashtbl.add annotations "sos_blk_mem_alloc" 
+      [Some "sos_claim"; None; None; None];
+
+    Hashtbl.add annotations "ker_free" 
+      [None; Some "sos_release"];
+
+    Hashtbl.add annotations "sos_blk_mem_free" 
+      [None; Some "sos_release"; None];
+
+    Hashtbl.add annotations "post_long" 
+      [None; None; None; None; None; Some "sos_may_release"; None];
+
+    Hashtbl.add annotations  "post_link"
+      [None; None; None; None; None; Some "sos_may_release"; None; None];
+
+    Hashtbl.add annotations  "post_auto"
+      [None; None; None; None; None; Some "sos_may_release"; None; None];
+
+    Hashtbl.add annotations  "post_net"
+      [None; None; None; None; None; Some "sos_may_release"; None; None];
+
+    Hashtbl.add annotations  "post_uart"
+      [None; None; None; None; None; Some "sos_may_release"; None; None];
+
+    Hashtbl.add annotations  "post_i2c"
+      [None; None; None; None; None; Some "sos_may_release"; None; None];
+
+    Hashtbl.add annotations  "post_spi"
+      [None; None; None; None; None; Some "sos_may_release"; None; None];
+
+    Hashtbl.add annotations  "ker_msg_take_data"
+      [Some "sos_claim"; None; None];
+
+    Hashtbl.add annotations  "msg_duplicate"
+      [Some "sos_claim"; None];
+
+    Hashtbl.add annotations  "msg_create"
+      [Some "sos_claim"];
+
+    Hashtbl.add annotations  "handle_incoming_msg"
+      [None; Some "sos_release"; None];
+
+    Hashtbl.add annotations  "sched_msg_alloc"
+      [None; Some "sos_release"];
+
+    Hashtbl.add annotations  "mq_enqueue"
+      [None; None; Some "sos_release"];
+
+    Hashtbl.add annotations  "ker_cam_add"
+      [None; None; Some "sos_release"];
+
+    Hashtbl.add annotations  "sos_msg_dispatch"
+      [None; Some "sos_release"];
+
+    Hashtbl.add annotations  "pop_new_mod_op"
+      [Some "sos_claim"; None];
+
+    annotations
+;;
+
+
+
 let feature : featureDescr = {
   fd_name = "addAnnotations";
   fd_enabled = ref true;
   fd_description = "Add annotations to function prototypes.";
   fd_extraopt = 
     [
-      
       ("--aa_dbg_verbose", Arg.Unit (fun _ -> dbg_verbose := true),
        "Enable more vebose debugging");
-    
     ];
   fd_doit = 
-
-    (function (f:file) ->
-      
-      Hashtbl.add annotations "ker_malloc" 
-        [Some "sos_claim"; None; None];
-      
-      Hashtbl.add annotations "sos_blk_mem_alloc" 
-        [Some "sos_claim"; None; None; None];
-      
-      Hashtbl.add annotations "ker_free" 
-        [None; Some "sos_release"];
-      
-      Hashtbl.add annotations "sos_blk_mem_free" 
-        [None; Some "sos_release"; None];
-      
-      Hashtbl.add annotations "post_long" 
-        [None; None; None; None; None; Some "sos_may_release"; None];
-      
-      Hashtbl.add annotations  "post_link"
-        [None; None; None; None; None; Some "sos_may_release"; None; None];
-      
-      Hashtbl.add annotations  "post_auto"
-        [None; None; None; None; None; Some "sos_may_release"; None; None];
-      
-      Hashtbl.add annotations  "post_net"
-        [None; None; None; None; None; Some "sos_may_release"; None; None];
-      
-      Hashtbl.add annotations  "post_uart"
-        [None; None; None; None; None; Some "sos_may_release"; None; None];
-      
-      Hashtbl.add annotations  "post_i2c"
-        [None; None; None; None; None; Some "sos_may_release"; None; None];
-      
-      Hashtbl.add annotations  "post_spi"
-        [None; None; None; None; None; Some "sos_may_release"; None; None];
-
-      Hashtbl.add annotations  "ker_msg_take_data"
-        [Some "sos_claim"; None; None];
-      
-      Hashtbl.add annotations  "msg_duplicate"
-        [Some "sos_claim"; None];
-      
-      Hashtbl.add annotations  "msg_create"
-        [Some "sos_claim"];
-      
-      Hashtbl.add annotations  "handle_incoming_msg"
-        [None; Some "sos_release"; None];
-      
-      Hashtbl.add annotations  "sched_msg_alloc"
-        [None; Some "sos_release"];
-
-      Hashtbl.add annotations  "mq_enqueue"
-        [None; None; Some "sos_release"];
-      
-      Hashtbl.add annotations  "ker_cam_add"
-        [None; None; Some "sos_release"];
-      
-      Hashtbl.add annotations  "sos_msg_dispatch"
-        [None; Some "sos_release"];
-      
-      Hashtbl.add annotations  "pop_new_mod_op"
-        [Some "sos_claim"; None];
-      
-
-      
-      
-      let aaVisitor = new addAnnotationsVisitor in
-        iterGlobals f (addAttributes f.globals)
-        (*visitCilFileSameGlobals aaVisitor f*)
-    );
-
+    (function (f:file) -> 
+      iterGlobals f (addAttributes (getAnnotations ())));
   fd_post_check = true;
 }
 
