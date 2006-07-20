5d411b2c9e36e4459e9000d318af857c8dd1291c
rshea <rshea@73ef524f-9218-0410-8865-d28d8c259d7e>
2006-07-20 20:01:12 +0000

    Finished cleaning up addAnnotations.  Unit test checkes that it can read in a configuration file by testing that hash table has 14 enteries (same as config file).  It would be nice to have some CIL based tests also...

--- src/memCheck/addAnnotations.ml
+++ src/memCheck/addAnnotations.ml
@@ -1,9 +1,9 @@
-(* 
- * File: addAnnotations
- * Author: Roy Shea <roy@cs.ucla.edu>
- * Date: 2/06
- * Purpose: Add annotations to specific function prototypes
- *)
+(** 
+  * File: addAnnotations
+  * Date: 2/06, 7/06
+  * Purpose: Add annotations to specific function prototypes
+  * @author Roy Shea <roy@cs.ucla.edu>
+  *)
 
 open Pretty
 open Cil
@@ -11,16 +11,13 @@ open Cil
 module E = Errormsg
 
 let dbg_verbose = ref false
+let config_file = ref "config.txt"
 
-let addAttributes annotations global =
 
-  (* Extract fundec and varinfo from global *)
-  let (fdop, vop) = 
-    match global with
-        GVarDecl (v, _) -> (None, Some v)
-      | GFun (f, _) -> (Some f, Some f.svar)
-      | _ -> (None, None)
-  in
+(** Update function [gfun]'s type with attributes described by the list
+  * [attributes].
+  *)
+let addFunAttributes gFun attributes =
 
   (* Return a new type that is the old type with an additional attribute set *)
   let newType t attr = 
@@ -37,168 +34,193 @@ let addAttributes annotations global =
       | TBuiltin_va_list a -> TBuiltin_va_list (addAttribute attr a)
   in
 
-  (* Update the varinfo with a new annotations if needed *)
-  let newFunTypeOp = 
-    match vop with
-        Some v ->
-          begin
-            match v.vtype with
-                TFun (t, ftlop, x, y) ->
+  (* Seperate new attributes into return and formamls *)
+  let (returnAttr, formalAttrL) = match attributes with
+      [] -> E.s (E.error "addFunAttributes: needs non-empty list in attributes\n")
+    | hd::[] -> (hd,[])
+    | hd::tl -> (hd,tl)
+  in
 
-                  if Hashtbl.mem annotations v.vname then (
+  (* Seperate function attributes into return and formals *)
+  let (returnType, formalTypeLOp, x, y) = 
+    match gFun.vtype with
+        TFun (t, Some ftl, x, y) -> (t, Some ftl, x, y)
+      | TFun (t, None, x, y) -> (t, None, x, y)
+      | _ -> E.s (E.error "addFunAnnotations: needs gFun to be of type TFun\n")
+  in
 
-                    let (returnAttr, formalAttrL) = match Hashtbl.find annotations v.vname with
-                        [] -> (None,[])
-                      | hd::[] -> (hd,[])
-                      | hd::tl -> (hd,tl)
-                    in
-
-                    let (returnType, formalTypeLOp, x, y) = 
-                      match v.vtype with
-                          TFun (t, Some ftl, x, y) -> (t, Some ftl, x, y)
-                        | TFun (t, None, x, y) -> (t, None, x, y)
-                        | _ -> E.s (E.bug "Expecting function type in addAnnotations\n")
-                    in
-
-                    let newReturnType = 
-                      match returnAttr with 
-                          Some a -> newType returnType (Attr (a, []))
-                        | None -> returnType
-                    in
-
-
-                    (* Assuming that the length of tl is the same as tlop *)
-                    let formalTypeLOp = 
-                      match formalTypeLOp with
-                          Some formalTypeL ->
-                            Some ( 
-                              List.map2 
-                                (fun newAttrOp oldT ->
-                                   match (newAttrOp, oldT) with
-                                     | (Some a, (x, y, oldA)) ->
-                                         (x, y, (addAttribute (Attr (a, [])) oldA))
-                                     | (None, _) ->
-                                         oldT
-                                ) 
-                                formalAttrL formalTypeL
-                            )
-                        | None -> None
-                    in
-                      Some (TFun (newReturnType, formalTypeLOp, x, y))
-                  ) else (
-                    None
-                  );
-              | _ -> None
-          end
-      | None -> None
+  (* Update the return attributes *)
+  let newReturnType = 
+    match returnAttr with 
+        Some a -> newType returnType (Attr (a, []))
+      | None -> returnType
   in
 
 
-    match (fdop, vop, newFunTypeOp) with
-        (_, None, _) -> 
-          if !dbg_verbose then
-            ignore (printf "Skipping nonglobal function\n");
-          ()
-      | (_, Some v, None) -> 
-          if !dbg_verbose then
-            ignore (printf "No prototype update needed for function %s: %a\n" v.vname d_type v.vtype);
-          ()
-      | (Some f, Some v, Some nft) -> 
-          setFunctionType f nft;
-          if !dbg_verbose then
-            ignore (printf "Prototype update for functin dec %s: %a\n" v.vname d_type v.vtype);
-          ()
-      | (None, Some v, Some nft) -> 
-          v.vtype <- nft;
-          if !dbg_verbose then
-            ignore (printf "Prototype update for extern func %s: %a\n" v.vname d_type v.vtype);
-          ()
-
-
-(** Get annotations from a file
-  * @todo Update this to read from a file rather than hardcode the values
-  * *)
-let getAnnotations () =
+  (* Walk through attribute option list and update the attributes of each variable*)
+  let newFormalTypeLOp = match formalTypeLOp with
+      Some formalTypeL ->
+        if not (List.length formalAttrL = List.length formalTypeL) then
+          E.s (E.error "addFunAttributes: Must have same sized formal and attribute lists\n");
+        Some (List.map2 
+                (fun newAttrOp oldT ->
+                   match (newAttrOp, oldT) with
+                     | (Some a, (x, y, oldA)) ->
+                         (x, y, (addAttribute (Attr (a, [])) oldA))
+                     | (None, _) ->
+                         oldT
+                ) 
+                formalAttrL 
+                formalTypeL
+        )
+    | None -> None
+  in
     
-  let annotations = (Hashtbl.create 5) in
-
-    Hashtbl.add annotations "ker_malloc" 
-      [Some "sos_claim"; None; None];
-
-    Hashtbl.add annotations "sos_blk_mem_alloc" 
-      [Some "sos_claim"; None; None; None];
-
-    Hashtbl.add annotations "ker_free" 
-      [None; Some "sos_release"];
-
-    Hashtbl.add annotations "sos_blk_mem_free" 
-      [None; Some "sos_release"; None];
-
-    Hashtbl.add annotations "post_long" 
-      [None; None; None; None; None; Some "sos_may_release"; None];
-
-    Hashtbl.add annotations  "post_link"
-      [None; None; None; None; None; Some "sos_may_release"; None; None];
-
-    Hashtbl.add annotations  "post_auto"
-      [None; None; None; None; None; Some "sos_may_release"; None; None];
-
-    Hashtbl.add annotations  "post_net"
-      [None; None; None; None; None; Some "sos_may_release"; None; None];
-
-    Hashtbl.add annotations  "post_uart"
-      [None; None; None; None; None; Some "sos_may_release"; None; None];
-
-    Hashtbl.add annotations  "post_i2c"
-      [None; None; None; None; None; Some "sos_may_release"; None; None];
-
-    Hashtbl.add annotations  "post_spi"
-      [None; None; None; None; None; Some "sos_may_release"; None; None];
-
-    Hashtbl.add annotations  "ker_msg_take_data"
-      [Some "sos_claim"; None; None];
-
-    Hashtbl.add annotations  "msg_duplicate"
-      [Some "sos_claim"; None];
-
-    Hashtbl.add annotations  "msg_create"
-      [Some "sos_claim"];
-
-    Hashtbl.add annotations  "handle_incoming_msg"
-      [None; Some "sos_release"; None];
-
-    Hashtbl.add annotations  "sched_msg_alloc"
-      [None; Some "sos_release"];
-
-    Hashtbl.add annotations  "mq_enqueue"
-      [None; None; Some "sos_release"];
-
-    Hashtbl.add annotations  "ker_cam_add"
-      [None; None; Some "sos_release"];
-
-    Hashtbl.add annotations  "sos_msg_dispatch"
-      [None; Some "sos_release"];
-
-    Hashtbl.add annotations  "pop_new_mod_op"
-      [Some "sos_claim"; None];
-
-    annotations
+    (TFun (newReturnType, newFormalTypeLOp, x, y))
 ;;
 
 
 
+(** Given a hash table of annotations [annotTable] and a global variable
+  * [global], if [global] is a function variable and has an entry in the hash
+  * table then add the annotations to its type.
+  *)
+let tryAddFunAttributes annotTable global =
+
+  (* Extract fundec and varinfo from global *)
+  let changedType = 
+    match global with
+        GVarDecl (v, _) -> 
+          begin
+            match v.vtype with
+                TFun (t, ftlop, x, y) ->
+                  if Hashtbl.mem annotTable v.vname then (
+                    let nft =addFunAttributes v (Hashtbl.find annotTable v.vname) in
+                      v.vtype <- nft;
+                      true
+                  ) else (
+                    false
+                  )
+              | _ -> false
+          end
+      | GFun (f, _) -> 
+          begin
+            match f.svar.vtype with
+                TFun (t, ftlop, x, y) ->
+                  if Hashtbl.mem annotTable f.svar.vname then (
+                    let nft = addFunAttributes f.svar (Hashtbl.find annotTable f.svar.vname) in
+                      setFunctionType f nft;
+                      true
+                  ) else (
+                    false
+                  )
+              | _ -> false
+          end
+      | _ -> false
+  in
+
+    if !dbg_verbose then (
+      if changedType then (
+        ignore (printf "Prototype update for functin %a\n" d_global global);
+      ) else (
+        ignore (printf "Skipping global %a\n" d_global global);
+      );
+    );
+
+    ()
+;;
+
+
+(** Get annotations from a file
+  * @todo Update this to read from a file rather than hardcode the values
+  *)
+let getAnnotations file =
+
+  (* Open a file and read each line into a list *)
+  let read_lines file =
+    let ch = open_in file in
+    let lines = ref [] in
+      try
+        while true do
+          lines := input_line ch :: !lines
+        done;
+        assert false
+      with
+          End_of_file ->
+            List.rev !lines
+  in
+
+  let annotations = (Hashtbl.create 5) in
+
+  (* Regular expressions to help parse each line of the file *)
+  let function_name_re = Str.regexp "^ *\\([a-zA-Z0-9_-]*\\)" in
+  let arguments_re = Str.regexp "(\\([a-zA-Z0-9_, -]*\\))" in 
+  let split_re = Str.regexp ", *" in 
+  
+  (* Given a line of a file with format:
+   *    function_name(formal_var_action1, formal_var_action2, ...)
+   * Extract the function name and list of foraml variable actions.  Add the
+   * list of actions to a hashtable indexed by the function name.
+   *)
+  let annotation_from_string s =
+    (try
+       ignore (Str.search_forward function_name_re s 0);
+       let fun_name = (Str.matched_group 1 s) in
+
+         ignore (Str.search_forward arguments_re s 0);
+         let string_args = Str.split split_re (Str.matched_group 1 s) in
+
+         let args = 
+           List.rev (
+             List.fold_left 
+               (fun alist a -> match a with
+                    "None" -> None::alist
+                  | "release" -> (Some "sos_release")::alist
+                  | "may_release" -> (Some "sos_may_release")::alist
+                  | "claim" -> (Some "sos_claim")::alist
+                  | _ -> 
+                      Errormsg.s (
+                        Errormsg.error "Unexpected label in %s: \"%s\"\n" file a
+                      )
+               )
+               [] 
+               string_args
+           )
+         in
+           Hashtbl.add annotations fun_name args
+     with
+         Not_found -> ();
+    );
+    ()
+  in
+
+  (* Parse the file and generate the hash table *)
+  let config = (read_lines file) in
+    List.iter (fun s -> annotation_from_string s) config;
+    annotations
+;;
+
+
+(** Feature description that allows this functionality to be easily added into a
+  * larger collection of tools.  Based on the feature mechanism implemented in
+  * CIL.
+  *)
 let feature : featureDescr = {
   fd_name = "addAnnotations";
   fd_enabled = ref true;
   fd_description = "Add annotations to function prototypes.";
   fd_extraopt = 
     [
+      ("--config", Arg.String (fun s -> config_file := s),
+       "Enable more vebose debugging");
       ("--aa_dbg_verbose", Arg.Unit (fun _ -> dbg_verbose := true),
        "Enable more vebose debugging");
     ];
   fd_doit = 
     (function (f:file) -> 
-      iterGlobals f (addAttributes (getAnnotations ())));
+      let annotations = getAnnotations !config_file in
+        iterGlobals f (tryAddFunAttributes annotations)
+    );
   fd_post_check = true;
 }
 
--- src/unitMemCheck/unitAddAnnotations.ml
+++ src/unitMemCheck/unitAddAnnotations.ml
@@ -1,21 +1,21 @@
 open OUnit
 open AddAnnotations
+
+open Pretty
+open Cil
   
 (* Read annotations from a file into a hash table *)
 let test_addAnnotations_getAnnotations = 
-  let annotations = AddAnnotations.getAnnotations () in
+  let annotations = AddAnnotations.getAnnotations "config.txt" in
     TestCase(fun _ -> assert_equal (Hashtbl.length annotations) 20);;
 
-(* Given a function generate a new function with annotations added *)
-let test_addAnnotations_addAttributes = 
-  TestCase(fun _ -> assert_equal "this" "fails");;
-
+(* Run all the tests *)
 let suite_addAnnotations = 
   TestLabel ("Add Annotations", 
              TestList [
-               TestLabel ("getAnnotations", test_addAnnotations_getAnnotations);
-               TestLabel ("addAttributes", test_addAnnotations_addAttributes)
+               TestLabel ("getAnnotations", test_addAnnotations_getAnnotations)
              ]
   );;
 
 let main = run_test_tt suite_addAnnotations
+
