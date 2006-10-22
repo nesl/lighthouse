30fb7d3864865655021ddd6ec0f51304addc9562
rshea <rshea@73ef524f-9218-0410-8865-d28d8c259d7e>
2006-10-22 21:43:33 +0000

    Collection of scripts used for evaluation of Lighthouse.
    getCvs: 
    - gets all versions of an SOS module from CVS
    - attempts to build each version
    - stores the .i and .c files in the base SOS directory

---
+++ documents/papers/07spots/evaluation/bin/getCvs.py
@@ -0,0 +1,209 @@
+#!/usr/bin/python
+
+import pexpect
+import os
+import sys
+import re
+
+# Poor man's debugging macro
+debugOn = True
+def DEBUG(out):
+    if debugOn:
+        print out
+
+
+################################################################
+# Simple utility functions to save typing
+################################################################
+
+##
+# Helper function to log the output of a make
+##
+def build(dir, command="", stdout="/dev/null", stderr="/dev/null"):
+    cmdMake = "make -C %s %s 1> %s 2> %s" % (dir, command, stdout, stderr)
+    os.system(cmdMake)
+
+
+##
+# Helper function to clean up the source tree
+##
+def clean(dir, file):
+    cmdClean = "make -C " + dir + " clean &> /dev/null"
+    os.system(cmdClean)
+    os.system("rm -f " + os.path.join(dir, file) + ".i")
+    os.system("rm -f " + os.path.join(dir, file) + ".o")
+    os.system("rm -f " + os.path.join(dir, file) + ".map")
+    os.system("rm -f " + os.path.join(dir, file) + ".lst")
+
+
+##
+# Helper function to clean up the source tree
+##
+def copy(dir, file, versionDate):
+    
+    versionString = versionDate.replace("/", "_").replace(" ", "_").replace(":", "_")
+   
+    cin = open(os.path.join(dir, file + ".c"), "rb")
+    
+    try:
+        iin = open(os.path.join(dir, file + ".i"), "rb")
+    except IOError:
+        print "Failed to find " + file + ".i"
+        cin.close()
+        return
+    
+    cout = open(file + "_" + versionString + ".c", "wb")
+    iout = open(file + "_" + versionString + ".i", "wb")
+
+    cout.write(cin.read())
+    iout.write(iin.read())
+
+    cin.close()
+    iin.close()
+    cout.close()
+    iout.close()
+
+
+################################################################
+# Interesting functions to get work done
+################################################################
+
+##
+# Function using pexpect to change a CVS repository to a specific dated
+# version.
+##
+def changeCvsVersion(root, date):
+
+    # Check out dated release (this assumes an anon checkout)
+    os.environ['CVS_RSH'] = 'ssh'
+    child = pexpect.spawn('cvs up -P -d -D"' + date + '"')
+    child.expect('password:')
+    child.sendline('anon')
+    child.expect(pexpect.EOF)
+    child.close()
+
+
+##
+# Build a specifc module
+##
+def buildModule(path, file, buildTarget="mica2"):
+    DEBUG("Starting: buildModule")
+    clean(path, file)
+    build(path, buildTarget)
+    DEBUG("End: buildModule")
+
+
+##
+# Get dates of all updates to a set module
+##
+def getVersionDates(path, file):
+
+    dates = []
+    
+    os.environ['CVS_RSH'] = 'ssh'
+    child = pexpect.spawn('cvs log ' + os.path.join(path, file) + ".c")
+    child.expect('password:')
+    child.sendline('anon')
+    
+    dateLine = re.compile("^date: ")
+    branchLine = re.compile("^revision \d+\.\d+\.\d+\.\d+")
+    
+    lines = child.readlines();
+    for line in lines:
+        
+        # Ignore dates of branches
+        m = branchLine.match(line)
+        if m:
+            break
+        
+        # Grab date from date lines
+        m = dateLine.match(line)
+        if m:
+            date = line.split(" ")[1:3]
+            dates.append(date[0] + " " + date[1][:-1])
+        
+    child.close()
+    
+    return dates;
+   
+
+##
+# Update module make files to include a .i in the mica2 bulid
+##
+def modifyMake():
+
+    try:
+        file = 'modules/Makerules'
+        inFile = open(file, 'r')
+        newFile = file + '.new'
+        outFile = open(newFile, 'w')
+
+        for line in inFile:
+            if '$(PLATFORM):' in line:
+                outFile.write('%.i : %.c\n')
+                outFile.write('\t$(CC) -E $(CFLAGS) -DCHECK $(INCDIR) $< -o $@\n')
+                outFile.write('\n')
+                outFile.write(line.rstrip() + ' $(PROJ).i\n')
+            else:
+                outFile.write(line)
+
+    except IOError:
+        file = 'modules/Makemod'
+        inFile = open(file, 'r')
+        newFile = file + '.new'
+        outFile = open(newFile, 'w')
+        
+        for line in inFile:
+            if 'mica2:' in line:
+                outFile.write('%.i : %.c\n')
+                outFile.write('\t$(CC) -E $(CFLAGS) -DCHECK $(INCDIR) $< -o $@\n')
+                outFile.write('\n')
+                outFile.write(line.rstrip() + ' $(PROJ).i\n')
+            else:
+                outFile.write(line)
+
+    os.rename(newFile, file)
+    inFile.close()
+    outFile.close()
+
+    return file
+
+
+################################################################
+# Go go driver function.
+################################################################
+
+if __name__ == '__main__':
+    
+    
+    if len(sys.argv) != 5:
+        print "Usage: " + sys.argv[0] + " <sosRoot> <modulePathe> <moduleName> <buildTarget>"
+        sys.exit()
+
+    # Get vital stats
+    sosRoot = sys.argv[1]
+    modulePath = sys.argv[2]
+    moduleName = sys.argv[3]
+    buildTarget = sys.argv[4]
+    
+    # Assume that this is run from the SOS directory so change into base directory
+    os.chdir(sosRoot)
+        
+    # Get all version dates that need to be checked out
+    dates = getVersionDates(modulePath, moduleName)
+    
+    count = 0
+    total = len(dates)
+    for versionDate in dates:
+
+        count = count + 1
+        #DEBUG("Checking " + moduleName + " from " + versionDate + ":")
+        DEBUG("Checking %s from %s (%d of %d)" % (moduleName, versionDate, count, total))
+        
+        changeCvsVersion(sosRoot, versionDate)
+        mkFile = modifyMake()
+        buildModule(modulePath, moduleName, buildTarget) 
+        copy(modulePath, moduleName, versionDate)
+        os.remove(mkFile)
+        
+
