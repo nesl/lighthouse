7382e3b1d47e2a5a02cf44634de3f31a4d82f11a
rshea <rshea@73ef524f-9218-0410-8865-d28d8c259d7e>
2006-10-26 01:34:50 +0000

    Current bug that is causing numerous false positives.

---
+++ src/unitMemCheck/isStoredUnit05.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+typedef struct {
+    int len;
+    int *data;
+    int flag;
+} Message;
+
+static Message store;
+
+void* ker_malloc(int size, int id) {
+    return malloc(size);
+}
+
+int main() {
+
+    unsigned int tmp;
+    tmp = (unsigned int)(&store) + 4; 
+    (*(int **)tmp) = (int*) ker_malloc(42, 1);
+    
+    return 0;
+}
+
