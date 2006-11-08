578c9cf4ee2fc3bc4d677494b49477b09506b9c4
rshea <rshea@73ef524f-9218-0410-8865-d28d8c259d7e>
2006-11-08 00:38:04 +0000

    Continuing to refine the case example.
    Added line numbers to dot c files.

--- documents/papers/07spots/GenericBaseM.c
+++ documents/papers/07spots/GenericBaseM.c
@@ -1,35 +1,34 @@
 \begin{footnotesize}
 \begin{verbatim}
-TOS_MsgPtr receive(TOS_MsgPtr received, bool fromUART) {
-    TOS_MsgPtr nextReceiveBuffer = received;
-
-    dbg(DBG_USR1, "GenericBase received %s packet\n",
-            fromUART ? "UART" : "radio");
-    if ((!sendPending) &&
-            (received->group == (TOS_AM_GROUP & 0xff))) {
-        result_t ok;
-
-        nextReceiveBuffer = ourBuffer;
-        ourBuffer = received;
-        dbg(DBG_USR1, "GenericBase forwarding packet to %s\n",
-                fromUART ? "radio" : "UART");
-        if (fromUART) {
-            call Leds.redToggle();
-            ok = call RadioSend.send(received);
-        } else {
-            call Leds.greenToggle();
-            received->addr = TOS_UART_ADDR;
-            ok = call UARTSend.send(received);
-        }
-        if (ok != FAIL) {
-            dbg(DBG_USR1, "GenericBase send pending\n");
-            sendPending = TRUE;
-        } else {
-            call Leds.yellowToggle();
-        }
-    }
-    return nextReceiveBuffer;
-}
+01 TOS_MsgPtr receive(TOS_MsgPtr received, bool fromUART) {
+02     TOS_MsgPtr nextReceiveBuffer = received;
+03
+04     dbg(DBG_USR1, "GenericBase received %s packet\n",
+05             fromUART ? "UART" : "radio");
+06     if ((!sendPending) &&
+07             (received->group == (TOS_AM_GROUP & 0xff))) {
+08         result_t ok;
+09
+10         nextReceiveBuffer = ourBuffer;
+11         ourBuffer = received;
+12         dbg(DBG_USR1, "GenericBase forwarding packet to %s\n",
+13                 fromUART ? "radio" : "UART");
+14         if (fromUART) {
+15             call Leds.redToggle();
+16             ok = call RadioSend.send(received);
+17         } else {
+18             call Leds.greenToggle();
+19             received->addr = TOS_UART_ADDR;
+20             ok = call UARTSend.send(received);
+21         }
+22         if (ok != FAIL) {
+23             dbg(DBG_USR1, "GenericBase send pending\n");
+24             sendPending = TRUE;
+25         } else {
+26             call Leds.yellowToggle();
+27         }
+28     }
+29     return nextReceiveBuffer;
+30 }
 \end{verbatim}
 \end{footnotesize}
-
--- documents/papers/07spots/surge.c
+++ documents/papers/07spots/surge.c
@@ -1,35 +1,35 @@
 \begin{footnotesize}
 \begin{verbatim}
-int8_t surge_module(void *state, Message *msg)
-{
-  surge_state_t *s = (surge_state_t*)state;
-
-  switch (msg->type){
-  case MSG_DATA_READY: // Requested sensor data ready
-  {
-    SurgeMsg* pkt = 
-      (uint8_t*)ker_malloc(sizeof(SurgeMsg));
-    if (pkt == NULL) break; 
-    pkt->data = ... ; // ... set up message
-    post_long(TREE_ROUTING_PID, SURGE_MOD_PID, 
-              MSG_SEND_PACKET, sizeof(SurgeMsg), 
-              (void*)pkt, SOS_MSG_RELEASE);
-    break;
-  }
-  case MSG_TR_DATA_PKT: 
-  {
-    if (ker_id() == SURGE_BASE_STATION_ADDRESS){
-      uint8_t *payload = ker_msg_take_data(msg); 
-      post_net(SURGE_MOD_PID, SURGE_MOD_PID,
-               msg->type, msg->len, payload,
-               SOS_MSG_RELEASE, ker_uart_id());
-      return SOS_OK;
-    }
-    break;
-  }
-  case ...: { ...; break; } // other messages
-  }
-  return SOS_OK;
-}
+01 int8_t surge_module(void *state, Message *msg)
+02 {
+03   surge_state_t *s = (surge_state_t*)state;
+04
+05   switch (msg->type){
+06   case MSG_DATA_READY: // Requested sensor data ready
+07   {
+08     SurgeMsg* pkt =
+09       (uint8_t*)ker_malloc(sizeof(SurgeMsg));
+10     if (pkt == NULL) break;
+11     pkt->data = ... ; // ... set up message
+12     post_long(TREE_ROUTING_PID, SURGE_MOD_PID,
+13               MSG_SEND_PACKET, sizeof(SurgeMsg),
+14               (void*)pkt, SOS_MSG_RELEASE);
+15     break;
+16   }
+17   case MSG_TR_DATA_PKT:
+18   {
+19     if (ker_id() == SURGE_BASE_STATION_ADDRESS){
+20       uint8_t *payload = ker_msg_take_data(msg);
+21       post_net(SURGE_MOD_PID, SURGE_MOD_PID,
+22                msg->type, msg->len, payload,
+23                SOS_MSG_RELEASE, ker_uart_id());
+24       return SOS_OK;
+25     }
+26     break;
+27   }
+28   case ...: { ...; break; } // other messages
+29   }
+30   return SOS_OK;
+31 }
 \end{verbatim}
 \end{footnotesize}
