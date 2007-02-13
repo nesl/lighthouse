d85cd623ce8557e69dc5c892facd3178083f3a26
rshea <rshea@73ef524f-9218-0410-8865-d28d8c259d7e>
2007-02-13 20:57:19 +0000

    Basic webpage setup for Lighthouse using AsciiDoc

---
+++ documents/web/build-website.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+VERS="0.0.1"
+DATE="2007-02-13"
+
+# Leave the desired layout uncommented.
+LAYOUT=layout          # Tables based layout.
+
+ASCIIDOC_HTML="asciidoc --unsafe --backend=xhtml11 --attribute icons --attribute iconsdir=./images/icons --attribute=badges --attribute=revision=$VERS  --attribute=date=$DATE"
+
+$ASCIIDOC_HTML --conf-file=${LAYOUT}.conf --attribute iconsdir=./icons --attribute=index-only index.txt
+
