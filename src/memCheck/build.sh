ff5ee2543f931a25bee2ca519236171085225bc6
rshea <rshea@73ef524f-9218-0410-8865-d28d8c259d7e>
2007-02-13 19:46:23 +0000

    Updated build.sh to make it more general to other installations.

--- src/memCheck/build.sh
+++ src/memCheck/build.sh
@@ -1,25 +1,28 @@
-CIL_PATH=/home/rshea/mote/cil-1.3.5/obj/x86_LINUX
-echo "Assuming library is located in: $CIL_PATH"
+CIL_ROOT=/cil-1.3.5
+ARCH=x86_LINUX
+
+CIL_LIB=$CIL_ROOT/obj/$ARCH
+echo "Assuming library is located in: $CIL_LIB"
 
 echo "Building object files..."
-ocamlc -c -I $CIL_PATH memUtil.mli
-ocamlc -c -I $CIL_PATH memUtil.ml
-ocamlc -c -I $CIL_PATH isEquivalent.mli
-ocamlc -c -I $CIL_PATH isEquivalent.ml
-ocamlc -c -I $CIL_PATH mayAliasWrapper.mli
-ocamlc -c -I $CIL_PATH mayAliasWrapper.ml
-ocamlc -c -I $CIL_PATH addAnnotations.mli
-ocamlc -c -I $CIL_PATH addAnnotations.ml
-ocamlc -c -I $CIL_PATH makeOneCFG.mli
-ocamlc -c -I $CIL_PATH makeOneCFG.ml
-ocamlc -c -I $CIL_PATH state.ml
-ocamlc -c -pp "camlp4o " -I $CIL_PATH specParse.ml
-ocamlc -c -I $CIL_PATH apollo.ml
-ocamlc -c -I $CIL_PATH rocket.ml
+ocamlc -c -I $CIL_LIB memUtil.mli
+ocamlc -c -I $CIL_LIB memUtil.ml
+ocamlc -c -I $CIL_LIB isEquivalent.mli
+ocamlc -c -I $CIL_LIB isEquivalent.ml
+ocamlc -c -I $CIL_LIB mayAliasWrapper.mli
+ocamlc -c -I $CIL_LIB mayAliasWrapper.ml
+ocamlc -c -I $CIL_LIB addAnnotations.mli
+ocamlc -c -I $CIL_LIB addAnnotations.ml
+ocamlc -c -I $CIL_LIB makeOneCFG.mli
+ocamlc -c -I $CIL_LIB makeOneCFG.ml
+ocamlc -c -I $CIL_LIB state.ml
+ocamlc -c -pp "camlp4o " -I $CIL_LIB specParse.ml
+ocamlc -c -I $CIL_LIB apollo.ml
+ocamlc -c -I $CIL_LIB rocket.ml
 
 echo "Linking object files..."
 ocamlc \
-    -I $CIL_PATH unix.cma str.cma cil.cma -o rocket \
+    -I $CIL_LIB unix.cma str.cma cil.cma -o rocket \
     memUtil.cmo isEquivalent.cmo mayAliasWrapper.cmo addAnnotations.cmo \
     makeOneCFG.cmo state.cmo specParse.cmo apollo.cmo rocket.cmo
 
