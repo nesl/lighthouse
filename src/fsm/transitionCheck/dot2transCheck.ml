ebab11ae5f5e115ec5e253f2bbdbabf65117e418
rshea <rshea@73ef524f-9218-0410-8865-d28d8c259d7e>
2007-04-20 18:02:55 +0000

    Code to parse dot file and now generate edge constraints for transitions in a state machine.

--- src/fsm/transitionCheck/dot2transCheck.ml
+++ src/fsm/transitionCheck/dot2transCheck.ml
@@ -5,7 +5,7 @@ module H = Hashtbl;;
 
 type dot_table = (id, id list) H.t;;
 
-let mapping: dot_table = H.create 17
+let mapping: dot_table = H.create 17;;
 
 (* Helper function to add elements to a hash table *)
 let update_table a b table =
@@ -13,33 +13,80 @@ let update_table a b table =
     H.replace table a (b @ (H.find table a))
   else
     H.add table a b
+;;
 
 
 (* Print basic usage instructions *)
 let usage () =
   prerr_endline (Printf.sprintf "usage: %s <file>" Sys.argv.(0));
   exit 1
+;;
+
+
+type transition = {
+  from_func:string; 
+  to_func:string; 
+  pre:string;
+  post:string;
+}
+;;
+
+
+let get_transition (attrs: Odot.attr list): (string * string) = 
+
+  let pre_string = match Odot.attr_value (Simple_id "taillabel") attrs with
+      Some (Double_quoted_id str) -> str
+    | Some _
+    | None -> 
+        ignore (
+          Printf.printf "Unable to find taillabel in attribute list: %s\n" 
+            (string_of_attr_list attrs)
+        );
+        ""
+  in
+  
+  let post_string = match Odot.attr_value (Simple_id "headlabel") attrs with
+      Some (Double_quoted_id str) -> str
+    | Some _
+    | None -> 
+        ignore (
+          Printf.printf "Unable to find headlabel in attribute list: %s\n" 
+            (string_of_attr_list attrs)
+        );
+        ""
+  in
+
+    (pre_string, post_string)
+
+;;
 
 
 (* Print all nodes seen in the file *)
-let generate_table (g:graph) table =
+let generate_table (g:graph) table constraints =
 
   let nodes_from_stmt s =
     match s with
         Stmt_node ((id1, _), _) -> 
           update_table id1 [] table;
           ()
-      | Stmt_edge (Edge_node_id (id1, _), [Edge_node_id (id2, _)], _) -> 
+      | Stmt_edge (Edge_node_id (id1, _), [Edge_node_id (id2, _)], attrs) -> 
           update_table id1 [id2] table;
           update_table id2 [] table;
-          ()
+          let (pre, post) = get_transition attrs in
+            constraints := {from_func=(string_of_id id1); 
+                            to_func=(string_of_id id2); 
+                            pre=pre; 
+                            post=post} :: !constraints;
+            ()
       | _ -> prerr_endline (
           Printf.sprintf 
-            "unable to handle: %s <file>" 
+            "unable to handle statement: %s" 
             (string_of_stmt g.kind s)
         )
   in
+
     List.iter nodes_from_stmt g.stmt_list
+;;
   
 
     
@@ -53,6 +100,7 @@ let print_states table =
        Printf.printf "\n"
     ) 
     table
+;;
 
     
 (* Given the state mapping generate an enum *)
@@ -62,6 +110,7 @@ let print_enum table =
   H.iter (fun a b -> Printf.printf "    dfs_%s,\n" (string_of_id a)) table;
   Printf.printf "};\n";
   ()
+;;
     
     
 (* Print a cascading "else if" statement for a given state and its transitions
@@ -92,19 +141,40 @@ let print_transition_check table =
     Printf.printf "    return false;\n";
     Printf.printf "}\n";
     ()
+;;
+
+
+let print_constraints constraints =
+
+  let print_constraint edge =
+
+    Printf.printf "Calling %s after finishing function %s\n" edge.to_func edge.from_func;
+
+    Printf.printf "%s.pre() {\n" edge.to_func;
+    Printf.printf "%s\n" edge.pre;
+    Printf.printf "}\n\n";
+
+    Printf.printf "%s.post() {\n" edge.to_func;
+    Printf.printf "%s" edge.post;
+    Printf.printf "}\n\n";
+
+  in
+
+    List.iter print_constraint constraints
+;;
 
       
-
-
 (* Driver function *)    
 let main () =
 
   if Array.length Sys.argv < 2 then
     usage ();
 
+  let constraints = ref [] in
+
   let g = Odot.parse_file Sys.argv.(1) in
     
-    generate_table g mapping;
+    generate_table g mapping constraints;
 
     (* print_states mapping; *)
       
@@ -112,10 +182,15 @@ let main () =
     print_enum mapping;
     Printf.printf "\n";
     print_transition_check mapping;
-    
+   
+    Printf.printf "\n";
+    print_constraints !constraints;
+
     ()
+;;
   
 
 (* Do something!!! *)
-let _ = main ()
-
+let _ = 
+  main ()
+;;
